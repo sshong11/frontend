d2d7b40f429beaaf4a076a0c2de26dcddd57de76
sshong11 <sshong2016@gmail.com>
2021-10-30 11:55:05 -0400

    added alt tag to img

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -16,7 +16,7 @@ function Projects(props) {
         return projects.map((project) => (
             <div>
                 <h1>{project.name}</h1>
-                <img src={project.image} />
+                <img src={project.image} alt ="projpicture"/>
                 <a href={project.git}>
                     <button>Github</button>
                 </a>
